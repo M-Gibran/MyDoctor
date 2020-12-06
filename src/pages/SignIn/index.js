04ff5061927c44fc97f4be9287b6a89a14038784
Muhamad Gibran <49028961+M-Gibran@users.noreply.github.com>
2020-12-06 22:03:19 +0700

    add config react redux

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -5,27 +5,22 @@ import {Button, Gap, Input, Link, Loading} from '../../components';
 import {Firebase} from '../../config';
 import {colors, fonts, storeData, useForm} from '../../utils';
 import {showMessage} from 'react-native-flash-message';
+import {useDispatch, useSelector} from 'react-redux';
 
 const SignIn = ({navigation}) => {
-  const [form, SetFrom] = useForm({
-    email: '',
-    password: '',
-  });
-
-  const [loading, SetLoading] = useState(false);
+  const [form, SetFrom] = useForm({email: '', password: ''});
+  const dispatch = useDispatch();
 
   const Login = () => {
-    SetLoading(true);
+    dispatch({type: 'SET_LOADING', value: true});
     Firebase.auth()
       .signInWithEmailAndPassword(form.email, form.password)
       .then((res) => {
-        SetLoading(false);
-        console.log('SUCESSS BOS', res);
+        dispatch({type: 'SET_LOADING', value: false});
         Firebase.database()
           .ref(`users/${res.user.uid}/`)
           .once('value')
           .then((resDB) => {
-            console.log('DATA DARI FIREBASE', resDB.val());
             if (resDB.val()) {
               storeData('user', resDB.val());
               navigation.replace('MainApp');
@@ -34,7 +29,7 @@ const SignIn = ({navigation}) => {
       })
       .catch((err) => {
         const errorMessage = err.message;
-        SetLoading(false);
+        dispatch({type: 'SET_LOADING', value: false});
         showMessage({
           message: errorMessage,
           type: 'default',
@@ -44,37 +39,34 @@ const SignIn = ({navigation}) => {
       });
   };
   return (
-    <>
-      <View style={styles.container}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <ILlogo />
-          <Text style={styles.title}>Masuk dan mulai berkontribusi</Text>
-          <Gap height={40} />
-          <Input
-            text="Email Address"
-            value={form.email}
-            onChangeText={(value) => SetFrom('email', value)}
-          />
-          <Gap height={24} />
-          <Input
-            text="Password"
-            value={form.password}
-            onChangeText={(value) => SetFrom('password', value)}
-            secureTextEntry
-          />
-          <Link txt="Forgot My Password" size={12} />
-          <Gap height={40} />
-          <Button title="Sign In" onPress={Login} />
-          <Link
-            txt="Create New Account"
-            size={16}
-            align="center"
-            onPress={() => navigation.navigate('SignUp')}
-          />
-        </ScrollView>
-      </View>
-      {loading && <Loading />}
-    </>
+    <View style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <ILlogo />
+        <Text style={styles.title}>Masuk dan mulai berkontribusi</Text>
+        <Gap height={40} />
+        <Input
+          text="Email Address"
+          value={form.email}
+          onChangeText={(value) => SetFrom('email', value)}
+        />
+        <Gap height={24} />
+        <Input
+          text="Password"
+          value={form.password}
+          onChangeText={(value) => SetFrom('password', value)}
+          secureTextEntry
+        />
+        <Link txt="Forgot My Password" size={12} />
+        <Gap height={40} />
+        <Button title="Sign In" onPress={Login} />
+        <Link
+          txt="Create New Account"
+          size={16}
+          align="center"
+          onPress={() => navigation.navigate('SignUp')}
+        />
+      </ScrollView>
+    </View>
   );
 };
 
